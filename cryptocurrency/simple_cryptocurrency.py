b08ff08b0ccccb8973fbdd45cabd56e4ab1f7437
kadencho <kadensungbincho@gmail.com>
2021-12-14 20:48:24 +0900

    feat: Add connect_node POST API

--- cryptocurrency/simple_cryptocurrency.py
+++ cryptocurrency/simple_cryptocurrency.py
@@ -145,4 +145,19 @@ def add_transaction():
     }
     return jsonify(response), 201
 
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {
+        'message': 'All the nodes are now connected. The Hatcoin Blockchain now contains the nodes',
+        'total_message': list(blockchain.nodes) 
+    }
+    return jsonify(response), 201
+
+
 app.run(host = '0.0.0.0', port = 5000)
