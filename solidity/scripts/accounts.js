e05b8fa131e2454b1a30bf005c68ae814eb6901c
kadensungbincho <kadensungbincho@linecorp.com>
2022-10-23 20:39:24 +0900

    feat: More on contracts

---
+++ solidity/scripts/accounts.js
@@ -0,0 +1,9 @@
+(async () => {
+    let accounts = await web3.eth.getAccounts();
+    console.log(accounts.length);
+
+    let balance = await web3.eth.getBalance(accounts[0]);
+    console.log(balance);
+    let balanceInEth = web3.utils.fromWei(balance.toString(), "ether");
+    console.log(balanceInEth);
+})()
